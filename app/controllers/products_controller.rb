2aaa576445bd1b008b8bcffe95844f1c03ccd354
skolznev <skolznevdm@gmail.com>
2017-03-16 19:33:55 +0300

    0.0.6

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,19 +12,36 @@ class ProductsController < ApplicationController
 		@product = Product.new
 	end
 
+	def edit
+		@product = Product.find(params[:id])
+	end
+
 	def create
 		@product = Product.new(product_params)
 
-		@product.save
-		redirect_to @product
+		if @product.save
+			redirect_to @product
+		else
+			render 'new'
+		end
 	end
 
-	# def upload
-	# 	uploaded_io = params[:product][:picture]
-	# 	File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-	# 		file.write(uploaded_io.read)
-	# 	end
-	# end
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update(product_params)
+			redirect_to @product
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+
+		redirect_to products_path
+	end
 
 	private
 		def product_params
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+
+	#Settings for paperclip gem
 	has_attached_file :picture, styles: {}, default_url: "img4.png"
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+	#Fields validations 
+	validates :name, 	presence: true, 
+						length: { minimum: 3 }
+
 end
