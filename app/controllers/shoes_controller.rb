0a2227988b7086054e1f7dc7beed5485c99520bf
skolznev <skolznevdm@gmail.com>
2017-03-22 07:00:54 +0300

    0.1.1

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,14 +1,15 @@
 class ShoesController < ApplicationController
-  def index
-  	@products = Product.all
-  end
-  def show
-		@product = Product.find(params[:id])
+	def index
+		@products = Product.all
 	end
 	def new
 		@product = Product.new
 	end
-	def edit
-		@product = Product.find(params[:id])
+	def create
+		@product = Product.new(params[:name])
+
+		@product.save
+		Products.select(:name).map(&:name).uniq
 	end
+
 end
