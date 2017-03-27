973b007daa67f3d324187f670c64a17f5f8963bc
skolznev <skolznevdm@gmail.com>
2017-03-27 20:41:34 +0300

    0.1.5

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,63 @@
-alert("f");
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+window.onload = function() {
+	var slides = document.querySelectorAll('#slides .slide');
+	var currentSlide = 0;
+	var slideInterval = setInterval(nextSlide,3000);
 
-function nextSlide(){
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+	function nextSlide() {
+    	goToSlide(currentSlide+1);
+	}
+
+	function previousSlide() {
+	    goToSlide(currentSlide-1);
+	}
+
+	function goToSlide(n) {
+	    slides[currentSlide].className = 'slide';
+	    currentSlide = (n+slides.length)%slides.length;
+	    slides[currentSlide].className = 'slide showing';
+	}
+
+	var playing = true;
+	var pauseButton = document.getElementById('pause');
+
+	function pauseSlideshow() {
+	    pauseButton.innerHTML = 'Play';
+	    playing = false;
+	    clearInterval(slideInterval);
+	}
+
+	function playSlideshow() {
+	    pauseButton.innerHTML = 'Pause';
+	    playing = true;
+	    slideInterval = setInterval(nextSlide,3000);
+	}
+
+	pauseButton.onclick = function() {
+	    if(playing) {
+	    pauseSlideshow();
+	  } else {
+	    playSlideshow();
+	  }
+	}
+	var next = document.getElementById('next');
+	var previous = document.getElementById('previous');
+
+	next.onclick = function() {
+	    pauseSlideshow();
+	    nextSlide();
+	};
+	previous.onclick = function() {
+	    pauseSlideshow();
+	    previousSlide();
+	}
+	var controls = document.querySelectorAll('.controls');
+	for(var i=0; i<controls.length; i++){
+    	controls[i].style.display = 'inline-block';
+	}
+
 }
+
+
+
+
 
